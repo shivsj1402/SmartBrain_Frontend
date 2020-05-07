f9b9df4dcae03da30bb92640bfef8854ecc33a30
Shivkumar Jaiswal <shivsj1402@gmail.com>
2020-05-06 23:52:26 -0300

    Updates

---
+++ precache-manifest.ea17f4c348fcd0265485d70e56164832.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b36ab0f6d8dae88a1696c9db57b3ee1f",
+    "url": "/SmartBrain_Frontend/index.html"
+  },
+  {
+    "revision": "720136c47c4702b0ed8e",
+    "url": "/SmartBrain_Frontend/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "8358ea63fca184b96853",
+    "url": "/SmartBrain_Frontend/static/css/main.0379947e.chunk.css"
+  },
+  {
+    "revision": "720136c47c4702b0ed8e",
+    "url": "/SmartBrain_Frontend/static/js/2.b5b31c1c.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/SmartBrain_Frontend/static/js/2.b5b31c1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8358ea63fca184b96853",
+    "url": "/SmartBrain_Frontend/static/js/main.50200c86.chunk.js"
+  },
+  {
+    "revision": "23c77315759c65c1c004",
+    "url": "/SmartBrain_Frontend/static/js/runtime-main.857abf4b.js"
+  },
+  {
+    "revision": "cc146e655c21bdb089e7a0f100c90b77",
+    "url": "/SmartBrain_Frontend/static/media/brainLogo.cc146e65.jpg"
+  }
+]);
